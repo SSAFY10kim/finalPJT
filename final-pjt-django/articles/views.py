5def7bf6564dfd301cc16544fefa8934be322196
SSAFY10kim <ehddud0534@gmail.com>
2023-11-15 09:10:30 +0900

    Update views.py

--- final-pjt-django/articles/views.py
+++ final-pjt-django/articles/views.py
@@ -11,11 +11,3 @@ API_KEY = settings.MONEY_API_KEY
 @api_view(['GET'])
 def money_data(request):
     url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
-    params = {
-        'authkey' : API_KEY,
-        'data' : 'AP01'
-    }
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    return Response(data)
